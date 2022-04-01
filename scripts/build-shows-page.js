94682d5b594096f9f0c4781e341a3cf635f343f6
Joanne Lin <joannelin@MacBook-Pro.local>
2022-04-01 08:56:14 -0700

    added the hover and selected functionality

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -34,7 +34,7 @@ let shows = [
     }
 ]
 
-  let showsSection=(showsObject, unorderedList)=>{
+  let showsSection=(showsObject, showsTable)=>{
       
           let dividerNode = document.createElement('div');
           dividerNode.classList.add("shows-section__divider");
@@ -43,23 +43,34 @@ let shows = [
 
           //when the row is selected or active
 
-          innerContainerNode.classList.add("shows-section__inner-container--active")
-       
+        //   innerContainerNode.classList.add("shows-section__inner-container--active")
+           
+          let dateContainerNode = document.createElement('div');
+          dateContainerNode.classList.add("shows-section__column-container");
           let dateHeaderNode = document.createElement('li');
           dateHeaderNode.classList.add("shows-section__li");
           let dateNode = document.createElement ('div');
           dateNode.classList.add("shows-section__date");
 
+          let venueContainerNode = document.createElement('div');
+          venueContainerNode.classList.add("shows-section__column-container");
           let venueHeaderNode = document.createElement('li');
           venueHeaderNode.classList.add("shows-section__li");
           let venueNode = document.createElement ('div');
           venueNode.classList.add("shows-section__venue");
 
+          let locationContainerNode=document.createElement('div');
+          locationContainerNode.classList.add("shows-section__column-container");
           let locationHeaderNode=document.createElement('li');
           locationHeaderNode.classList.add("shows-section__li");
           let locationNode = document.createElement('div');
           locationNode.classList.add("shows-section__location");
         
+          
+          let buttonContainerNode=document.createElement('div');
+          buttonContainerNode.classList.add("shows-section__column-container");
+          let emptyNode=document.createElement('li');
+          emptyNode.classList.add("shows-section__empty");
           let buttonNode = document.createElement('button');
           buttonNode.classList.add("shows-section__button");
           buttonNode.innerText = "BUY TICKETS";
@@ -70,26 +81,44 @@ let shows = [
           venueNode.innerText = showsObject.venue;
           locationHeaderNode.innerText= "LOCATION";
           locationNode.innerText = showsObject.location;
+          emptyNode.innerText= '\u00A0';
+
+        
   
-        //   avatarContainerNode.innerHTML = avatarNode.outerHTML;
-          innerContainerNode.innerHTML = dateHeaderNode.outerHTML + dateNode.outerHTML + venueHeaderNode.outerHTML + venueNode.outerHTML + locationHeaderNode.outerHTML + locationNode.outerHTML + buttonNode.outerHTML;
-        //   innerContainerNode.innerHTML= avatarContainerNode.outerHTML + inputContainerNode.outerHTML;
+          dateContainerNode.innerHTML = dateHeaderNode.outerHTML + dateNode.outerHTML;
+          venueContainerNode.innerHTML = venueHeaderNode.outerHTML + venueNode.outerHTML;
+          locationContainerNode.innerHTML = locationHeaderNode.outerHTML + locationNode.outerHTML;
+          buttonContainerNode.innerHTML = emptyNode.outerHTML + buttonNode.outerHTML;
+          innerContainerNode.innerHTML = dateContainerNode.outerHTML + venueContainerNode.outerHTML + locationContainerNode.outerHTML + buttonContainerNode.outerHTML;
           
-          unorderedList.appendChild(innerContainerNode);
-          unorderedList.appendChild(dividerNode);
-    
-  }
+          showsTable.appendChild(innerContainerNode);
+          showsTable.appendChild(dividerNode);
+
+
+          
+            innerContainerNode.addEventListener("click", function(){
+                let selectedRow = document.getElementsByClassName("shows-section__inner-container--active");
+                for (let i=0;i<selectedRow.length;i++){
+                    selectedRow[i].classList.remove("shows-section__inner-container--active");
+                }
+                innerContainerNode.classList.add("shows-section__inner-container--active")
+          });
+        }
   
   let displayShows = ()=>{
-      let unorderedList = document.querySelector('#table');
+      let showsTable = document.querySelector('#table');
      
       for (let i=0;i<shows.length;i++){
           let showsObject = shows[i]
-          showsSection(showsObject, unorderedList);
+          showsSection(showsObject, showsTable);
           
       } 
   }
   
   displayShows();
-  
-  
+
+
+
+
+
+ 
