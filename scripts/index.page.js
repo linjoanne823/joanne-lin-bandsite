5c49841c6d8b14d867b151490c66f5004f9d6fae
Joanne Lin <joannelin@MacBook-Pro.local>
2022-04-08 12:10:07 -0700

    added axios get and post

--- scripts/index.page.js
+++ scripts/index.page.js
@@ -1,115 +1,212 @@
+// const itemForm = document.getElementById('itemForm');
+// const dynamicContent = document.getElementById('comments-list');
+
+
+//     axios.get('https://project-1-api.herokuapp.com/comments?api_key=037ccb3f-b3ad-450d-b10c-5c8d2ebdab07')
+//     .then((result) =>{
+//         console.log(result.data);
+//         displayComment(result.data)
+//     })
+
+//     .catch(error =>{
+//         console.log(error);
+//     });
+
+
+// itemForm.addEventListener('submit', function (event){
+//   event.preventDefault();
+//   const nameInputVal = event.target.nameInput.value; 
+//   event.target.nameInput.value = '';
+//   const commentInputVal=event.target.commentInput.value;
+//   event.target.commentInput.value = '';
+
+//   let newCommentObject = {
+//       name:nameInputVal,
+//       timestamp: new Date(),
+//       comment:commentInputVal,
+//   }
+
+//   let month = newCommentObject.timestamp.getMonth() +1;
+//   let day = newCommentObject.timestamp.getDate();
+//   let year = newCommentObject.timestamp.getFullYear();
+
+//   if(day<10){
+//       day='0'+day
+//   };
+
+//   if(month<10){
+//       month = '0' + month;
+//   }
+
+//   newCommentObject.timestamp = month + '/' + day + '/' + year;
+
+//   comments.unshift(newCommentObject);
+//   let commentList = document.querySelector('#comments-list');
+//   for (let i=0;i<comments.length;i++){
+//     let commentObject = comments[i]
+//     displayComment(commentObject);
+
+//   }
+// });
+
+// let addComment=(commentObject)=>{
+    
+//         let dividerNode = document.createElement('div');
+//         dividerNode.classList.add("comment-section__divider");
+//         let outerContainerNode = document.createElement('div');
+//         outerContainerNode.classList.add("comment-section__outer-container");
+//         let innerContainerNode = document.createElement('div');
+//         innerContainerNode.classList.add("comment-section__inner-container");
+//         let avatarContainerNode = document.createElement('div');
+//         avatarContainerNode.classList.add("comment-section__avatar-container--previous");
+//         let avatarNode = document.createElement('div');
+//         avatarNode.classList.add("comment-section__avatar");
+//         let inputContainerNode = document.createElement('div');
+//         inputContainerNode.classList.add("comment-section__input-container");
+//         let rowContainerNode=document.createElement('div');
+//         rowContainerNode.classList.add("comment-section__row-container");
+//         let nameNode = document.createElement ('span');
+//         nameNode.classList.add("comment-section__name");
+//         let timeStampNode = document.createElement ('span');
+//         timeStampNode.classList.add("comment-section__timestamp");
+//         let commentNode = document.createElement('li');
+//         commentNode.classList.add("comment-section__comment");
+
+//         nameNode.innerText = commentObject.name;
+//         timeStampNode.innerText = commentObject.timestamp;
+//         commentNode.innerText = commentObject.comment;
+
+//         avatarContainerNode.innerHTML = avatarNode.outerHTML;
+//         rowContainerNode.innerHTML= nameNode.outerHTML + timeStampNode.outerHTML
+//         inputContainerNode.innerHTML = rowContainerNode.outerHTML + commentNode.outerHTML;
+//         innerContainerNode.innerHTML= avatarContainerNode.outerHTML + inputContainerNode.outerHTML;
+        
+//         outerContainerNode.innerHTML= innerContainerNode.outerHTML + dividerNode.outerHTML;
+
+//         return outerContainerNode;        
+// }
+
+// let displayComment = (comments)=>{
+//     let commentList = document.querySelector('#comments-list');
+//     commentList.innerHTML = '';
+//     let dividerNode = document.createElement('div');
+//     dividerNode.classList.add("comment-section__divider");
+//     commentList.appendChild(dividerNode);
+
+//     for (let i=0;i<comments.length;i++){
+//         let commentObject = comments[i];
+//         commentList.appendChild(addComment(commentObject));
+//     } 
+    
+// }
+
+// displayComment();
+
+
+
 const itemForm = document.getElementById('itemForm');
 const dynamicContent = document.getElementById('comments-list');
 
-let comments = [
-    {
-        name: 'Connor Walton',
-        timeStamp: '02/17/2021',
-        comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.'
-    },
-    {
-        name: 'Emilie Beach',
-        timeStamp: '01/09/2021',
-        comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.'
-    },
-    {
-        name: 'Miles Acosta',
-        timeStamp: '12/20/2020',
-        comment:"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-    }
-];
-
-itemForm.addEventListener('submit', function (event){
-  event.preventDefault();
-  const nameInputVal = event.target.nameInput.value; 
-  event.target.nameInput.value = '';
-  const commentInputVal=event.target.commentInput.value;
-  event.target.commentInput.value = '';
-
-  let newCommentObject = {
-      name:nameInputVal,
-      timeStamp: new Date(),
-      comment:commentInputVal,
-  }
-
-  let month = newCommentObject.timeStamp.getMonth() +1;
-  let day = newCommentObject.timeStamp.getDate();
-  let year = newCommentObject.timeStamp.getFullYear();
-
-  if(day<10){
-      day='0'+day
-  };
-
-  if(month<10){
-      month = '0' + month;
-  }
-
-  newCommentObject.timeStamp = month + '/' + day + '/' + year;
-
-  comments.unshift(newCommentObject);
-  let commentList = document.querySelector('#comments-list');
-  for (let i=0;i<comments.length;i++){
-    let commentObject = comments[i]
-    displayComment(commentObject);
-
-  }
+//get request 
+axios.get('https://project-1-api.herokuapp.com/comments?api_key=037ccb3f-b3ad-450d-b10c-5c8d2ebdab07')
+.then((result) =>{
+    const commentsFromServer = result.data;
+    console.log(commentsFromServer);
+    displayComment(commentsFromServer)
+})
+.catch(error =>{
+    console.log(error);
 });
 
-let addComment=(commentObject)=>{
+//this is the HTML form. userInput is the new comment/user input 
+itemForm.addEventListener('submit', function (event){
+    event.preventDefault();
+    const nameInputVal = event.target.nameInput.value;
+    event.target.nameInput.value = '';
+    const commentInputVal=event.target.commentInput.value;
+    event.target.commentInput.value = '';
+    let userInput = {
+        name:nameInputVal,
+        timestamp: new Date(),
+        comment:commentInputVal,
+    }
+    let month = userInput.timestamp.getMonth() +1;
+    let day = userInput.timestamp.getDate();
+    let year = userInput.timestamp.getFullYear();
+  
+    if(day<10){
+        day='0'+day
+    };
+
+    if(month<10){
+        month = '0' + month;
+    };
+  
+    userInput.timestamp = month + '/' + day + '/' + year;
+//post request 
+
+    axios.post('https://project-1-api.herokuapp.com/comments?api_key=037ccb3f-b3ad-450d-b10c-5c8d2ebdab07',{
+        "name": userInput.name,
+        "comment": userInput.comment
+    })
+    .then((result, userInput) => {
+        const commentsFromServer = result.data;
+        console.log (commentsFromServer.unshift(userInput));
+        displayComment(commentsFromServer.forEach(userInput))
+    })
+    .catch(error => {
+        console.log(error);
+    })
+   
     
-        let dividerNode = document.createElement('div');
-        dividerNode.classList.add("comment-section__divider");
-        let outerContainerNode = document.createElement('div');
-        outerContainerNode.classList.add("comment-section__outer-container");
-        let innerContainerNode = document.createElement('div');
-        innerContainerNode.classList.add("comment-section__inner-container");
-        let avatarContainerNode = document.createElement('div');
-        avatarContainerNode.classList.add("comment-section__avatar-container--previous");
-        let avatarNode = document.createElement('div');
-        avatarNode.classList.add("comment-section__avatar");
-        let inputContainerNode = document.createElement('div');
-        inputContainerNode.classList.add("comment-section__input-container");
-        let rowContainerNode=document.createElement('div');
-        rowContainerNode.classList.add("comment-section__row-container");
-        let nameNode = document.createElement ('span');
-        nameNode.classList.add("comment-section__name");
-        let timeStampNode = document.createElement ('span');
-        timeStampNode.classList.add("comment-section__timestamp");
-        let commentNode = document.createElement('li');
-        commentNode.classList.add("comment-section__comment");
+});
 
-        nameNode.innerText = commentObject.name;
-        timeStampNode.innerText = commentObject.timeStamp;
-        commentNode.innerText = commentObject.comment;
 
-        avatarContainerNode.innerHTML = avatarNode.outerHTML;
-        rowContainerNode.innerHTML= nameNode.outerHTML + timeStampNode.outerHTML
-        inputContainerNode.innerHTML = rowContainerNode.outerHTML + commentNode.outerHTML;
-        innerContainerNode.innerHTML= avatarContainerNode.outerHTML + inputContainerNode.outerHTML;
-        
-        outerContainerNode.innerHTML= innerContainerNode.outerHTML + dividerNode.outerHTML;
 
-        return outerContainerNode;        
-}
-
-let displayComment = ()=>{
-    let commentList = document.querySelector('#comments-list');
-    commentList.innerHTML = '';
+let addComment=(commentToAdd)=>{
     let dividerNode = document.createElement('div');
     dividerNode.classList.add("comment-section__divider");
-    commentList.appendChild(dividerNode);
-   
-    for (let i=0;i<comments.length;i++){
-        let commentObject = comments[i]
-        commentList.appendChild(addComment(commentObject));
+    let outerContainerNode = document.createElement('div');
+    outerContainerNode.classList.add("comment-section__outer-container");
+    let innerContainerNode = document.createElement('div');
+    innerContainerNode.classList.add("comment-section__inner-container");
+    let avatarContainerNode = document.createElement('div');
+    avatarContainerNode.classList.add("comment-section__avatar-container--previous");
+    let avatarNode = document.createElement('div');
+    avatarNode.classList.add("comment-section__avatar");
+    let inputContainerNode = document.createElement('div');
+    inputContainerNode.classList.add("comment-section__input-container");
+    let rowContainerNode=document.createElement('div');
+    rowContainerNode.classList.add("comment-section__row-container");
+    let nameNode = document.createElement ('span');
+    nameNode.classList.add("comment-section__name");
+    let timeStampNode = document.createElement ('span');
+    timeStampNode.classList.add("comment-section__timestamp");
+    let commentNode = document.createElement('li');
+    commentNode.classList.add("comment-section__comment");
+    nameNode.innerText = commentToAdd.name;
+    timeStampNode.innerText = commentToAdd.timestamp;
+    commentNode.innerText = commentToAdd.comment;
+    avatarContainerNode.innerHTML = avatarNode.outerHTML;
+    rowContainerNode.innerHTML= nameNode.outerHTML + timeStampNode.outerHTML
+    inputContainerNode.innerHTML = rowContainerNode.outerHTML + commentNode.outerHTML;
+    innerContainerNode.innerHTML= avatarContainerNode.outerHTML + inputContainerNode.outerHTML;
+    outerContainerNode.innerHTML= innerContainerNode.outerHTML + dividerNode.outerHTML;
         
-    } 
+    return outerContainerNode;
 }
 
-displayComment();
-
-
-
+let displayComment = (commentsToDisplay)=>{
+    let displayContainer = document.querySelector('#comments-list'); 
+    displayContainer.innerHTML = '';
+    let dividerNode = document.createElement('div');
+    dividerNode.classList.add("comment-section__divider");
+    displayContainer.appendChild(dividerNode);
+    commentsToDisplay.forEach(commentToDisplay => {
+        displayContainer.appendChild(addComment(commentToDisplay));
+        
+    });
+}
 
 
 
